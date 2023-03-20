8ffb00a3ab402244b592559bf8158b21b5818d64
Neema Totonchi <ntotonchi1@se14l0120-15.ad.bcit.ca>
2023-03-20 14:34:30 -0700

    initial commit

---
+++ components/BarChart/index.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+import styles from './BarChart.module.css';
+
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+export default function BarChart(){
+
+    const [chartData, setChartData] = useState({
+        datasets: []
+    })
+
+    const [chartOptions, setChartOptions] = useState({})
+
+    useEffect(() => {
+        setChartData({
+            labels: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
+            datasets: [
+                {
+                    label: 'Sales $',
+                    data: [15323, 34253, 5000, 24520, 10532, 30143, 13896],
+                    borderColor: 'rgb(53,162,235)',
+                    backgroundColor: 'rgba(53,162,235,0.4)'
+                }
+            ]
+        })
+
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'top'
+                },
+                title: {
+                    display: true,
+                    text: "Daily Revenue"
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        })
+    }, [])
+
+    return(
+        <>
+        <div className={styles.container}>
+            <Bar data={chartData} options={chartOptions}/>
+        </div>
+        </>
+    )
+}
